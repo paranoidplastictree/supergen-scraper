6c445fecc51cf73ad148bb7d60ac37149a83ddd1
Tim Storms <tim.storms@avasure.com>
2019-11-14 08:29:27 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+String.prototype.initCap = function () {
+  return this.toLowerCase().replace(/(?:^|\s)[a-z]/g, function (m) {
+     return m.toUpperCase();
+  });
+};
+
+function readScrapeFromFile() {
+  var fs = require('fs'),
+    path = require('path'),    
+    filePath = path.join(__dirname, 'scraped-supergens.html');
+  return fs.readFileSync(filePath, {encoding:'utf-8', flag:'r'});
+}
+
+function scrapeToFile(url) {
+  const options = {
+    uri: url,
+    transform: function (body) {
+      return cheerio.load(body);
+    }
+  };
+
+  rp(options)
+  .then(($) => {
+    const body = $('body');
+    fs.writeFile('scraped-supergens.html', body, (err) => {
+      if (err) throw err;
+      console.log('Scrape saved!');
+    });
+
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
+
+function addToList(newItems, targetList) {
+  newItems.forEach((item) => {
+    const itemInitCap = item.initCap();
+    if (!targetList.includes(itemInitCap)) {
+      targetList.push(itemInitCap);
+    }
+  });
+  return targetList;
+}
+
+function parseScrape() {
+  const body = readScrapeFromFile();
+  const $ = cheerio.load(body);
+  
+  // This assumes the following html structure for supergen links and corresponding descriptions. This may change in the future.
+  //<p class="_1qeIAgB0cPwnLhDF9XSiJM"><a href="http://goo.gl/CUyAxI" class="_3t5uN8xUmg0TOwRCOGQEcU" rel="noopener noreferrer" target="_blank">Aboard an Interstellar Spacecraft</a> <a href="https://redd.it/2j8gz2" class="_3t5uN8xUmg0TOwRCOGQEcU" rel="noopener noreferrer" target="_blank">(Link)</a></p>
+  //<ul class="_33MEMislY0GAlB78wL1_CR"><li class="_3gqTEjt4x9UIIpWiro7YXz"><p class="_1qeIAgB0cPwnLhDF9XSiJM">Sounds Used: Aircraft Cabin Noise, Binaural Beat Machine, Temple Bells, Twilight</p></li></ul>
+  
+  const links = $('p._1qeIAgB0cPwnLhDF9XSiJM a._3t5uN8xUmg0TOwRCOGQEcU:first-child'); // p a // a._3t5uN8xUmg0TOwRCOGQEcU
+  const supergenInfo = {
+    sounds: [],
+    supergens: []
+  };
+
+  $(links).each(function(i, elem) {
+    const sounds = $(elem).parent().next('ul._33MEMislY0GAlB78wL1_CR').find('li p').text().replace('Sounds Used: ', '').split(', ');
+    supergenInfo.sounds = addToList(sounds, supergenInfo.sounds).sort();
+    supergenInfo.supergens.push({
+      name: $(elem).text(),
+      href: $(elem).attr('href'),
+      sounds: sounds
+    });
+  });
+
+  fs.writeFileSync('supergens.js', JSON.stringify(supergenInfo));
+  console.log('Supergens json saved!');
+}
+
+//scrapeToFile(`https://www.reddit.com/r/MyNoise/comments/3hw95k/supergen_masterlist/`);
+parseScrape();
