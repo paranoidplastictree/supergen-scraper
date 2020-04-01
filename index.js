bc39a0f403db4975a1df9ad0c6b9f9ccf4b8e197
paranoidplastictree <timstorms@gmail.com>
2020-03-31 23:46:43 -0400

    begin scrape of mynoise

--- index.js
+++ index.js
@@ -16,34 +16,44 @@ function getMenuSelection() {
   console.log('///////////////////////////////');
   console.log('');
   console.log('Commands:');
-  console.log('scrape - Scrape Supergen Masterlist');
-  console.log('export - Export Supergen meta');
+  console.log('scrape masterlist - Scrape Supergen Masterlist');
+  console.log('export supergen meta - Export Supergen meta');
+  console.log('scrape mynoise - Scrape MyNoise Noise Machines');
   console.log('exit   - Exit');
 
+  const supergenMasterListUrl = `https://www.reddit.com/r/MyNoise/comments/3hw95k/supergen_masterlist/`;
+  const supergenScrapeFileName = 'scraped-supergens.html';
+  const myNoiseMachinesUrl = 'https://mynoise.net/noiseMachines.php';
+  const myNoiseScrapeFileName = 'scraped-noise-machines.html';
+  
   standard_input.on('data', function (data) {
     if(data === 'exit\r\n') {
       console.log("User input complete, program exit.");
       process.exit();
-    } else if(data === 'scrape\r\n') {
-      scrapeToFile();
-    } else if(data === 'export\r\n') {
-      parseScrape();
+    } else if(data === 'scrape masterlist\r\n') {
+      scrapeToFile(supergenMasterListUrl, supergenScrapeFileName);
+    } else if(data === 'export supergen meta\r\n') {
+      parseSupergenScrape(supergenScrapeFileName);
+    } else if(data === 'scrape mynoise\r\n') {
+      scrapeToFile(myNoiseMachinesUrl, myNoiseScrapeFileName);
+    } else if(data === 'export mynoise meta\r\n') {
+      parseMyNoiseScrape(myNoiseScrapeFileName);
     } else {
       console.log('Invalid input');
     }
   });
 }
 
-function readScrapeFromFile() {
+function readScrapeFromFile(sourceFileName) {
   var fs = require('fs'),
     path = require('path'),    
-    filePath = path.join(__dirname, 'scraped-supergens.html');
+    filePath = path.join(__dirname, sourceFileName);
   return fs.readFileSync(filePath, {encoding:'utf-8', flag:'r'});
 }
 
-function scrapeToFile(url) {
+function scrapeToFile(sourceUrl, targetFileName) {
   const options = {
-    uri: url,
+    uri: sourceUrl,
     transform: function (body) {
       return cheerio.load(body);
     }
@@ -52,9 +62,9 @@ function scrapeToFile(url) {
   rp(options)
   .then(($) => {
     const body = $('body');
-    fs.writeFile('scraped-supergens.html', body, (err) => {
+    fs.writeFile('', body, (err) => {
       if (err) throw err;
-      console.log('Supergen html scraped and saved!');
+      console.log('html scraped and saved!');
     });
 
   })
@@ -90,8 +100,8 @@ function addToList(newItems, targetList) {
   return result;
 }
 
-function parseScrape() {
-  const body = readScrapeFromFile();
+function parseSupergenScrape(sourceFileName) {
+  const body = readScrapeFromFile(sourceFileName);
   const $ = cheerio.load(body);
   
   // This assumes the following html structure for supergen links and corresponding descriptions. This may change in the future.
@@ -103,6 +113,111 @@ function parseScrape() {
     sounds: [],
     supergens: []
   };
+
+  $(links).each(function(i, elem) {
+    const sounds = $(elem).parent().next('ul._33MEMislY0GAlB78wL1_CR').find('li p').text().replace('Sounds Used: ', '').split(', ');
+    const result = addToList(sounds, supergenInfo.sounds);
+    supergenInfo.sounds = result.targetList;
+    
+    supergenInfo.supergens.push({
+      id: i,
+      name: $(elem).text(),
+      href: $(elem).attr('href'),
+      sounds: result.itemSounds
+    });
+  });
+
+  fs.writeFileSync('supergens.js', JSON.stringify(supergenInfo));
+  console.log('Supergens json saved!');
+}
+
+// TODO: Reuse this for both supergen sounds and noise machine categories
+function addNewItems(newItems, targetList) {
+  const result = { itemList: [] };
+
+  newItems.forEach((nextItem) => {
+    const nextItemInitCap = nextItem.initCap();
+    let matchFound = false;
+
+    targetList.forEach((targetListItem) => {
+      if (targetListItem.name === nextItemInitCap) {
+        matchFound = true;
+        result.itemList.push(targetListItem);
+        return;
+      }
+    });
+
+    if (!matchFound) {
+      const newItem = {id: targetList.length, name: nextItemInitCap};
+      targetList.push(newItem);
+      result.itemList.push(newItem);
+    }
+
+    result.targetList = targetList.sort((a, b) => (a.name > b.name) ? 1 : -1);
+    result.itemList = result.itemList.sort((a, b) => (a.name > b.name) ? 1 : -1);
+  });
+  return result;
+}
+
+function parseMyNoiseScrape(sourceFileName) {
+  const body = readScrapeFromFile(sourceFileName);
+  const $ = cheerio.load(body);
+  
+  // This assumes the following html structure for mynoise noise machine links and associated meta. This may change in the future.
+  // <div class="generator-list">
+  //   <h1>category title here</h1>  
+  //   <p>
+  //     <span class="DIM FOREST">
+  //       <img src="/Pix/fav.png" class="iFV" id="fFOREST" alt="favorite" onclick="favorite(&quot;FOREST&quot;)" title="Your favorites" style="display: inline; cursor: pointer;">
+  //       <a href="./NoiseMachines/primevalEuropeanForestSoundscapeGenerator.php" onmouseover="play(&quot;FOREST&quot;)" onmouseout="stop()" class="NB MN UFV FOREST hint" style="cursor: pointer;">Primeval Forest</a>&nbsp;
+  //       <img src="/Pix/ufav.png" class="iUFV" alt="patrons favorites" onclick="highlight(&quot;UFV&quot;)" style="cursor:pointer;">
+  //       <img src="/Pix/moon.png" class="iNB" alt="moon" onclick="highlight(&quot;NB&quot;)" style="cursor:pointer;">
+  //       <img src="/Pix/yin.png" class="iMN" alt="yin" onclick="highlight(&quot;MN&quot;)" style="cursor:pointer;"><br>
+  //     </span>
+  //     <span class="DIM SOMEOTHERNOISE">...</span>
+  //   </p>
+  // </div>
+  const noiseMachineCategories = [
+    { id: 'NB', name: 'Noise Blocker'},
+    { id: 'HC', name: 'Health Care'},
+    { id: 'ST', name: 'Sound Therapy'},
+    { id: 'MN', name: 'Meditation'},
+    { id: 'EE', name: 'Eerie'},
+    { id: 'TN', name: 'Tonal'},
+    { id: 'MU', name: 'Musical'},
+    { id: 'CL', name: 'Calibrated'},
+    { id: 'HP', name: 'Headphones'},
+    { id: 'UFV', name: 'Patron Favorite'},
+    { id: 'CU', name: 'Custom'}
+  ];
+  const noiseMachineInfo = {
+    listNames: [],
+    noiseMachineCategories: noiseMachineCategories,
+    noiseMachines: []
+  };
+
+  const generatorLists = $('div.generator-list');
+  $(generatorLists).each(function(i, elem) {
+    const title = $(elem).find('h1').text();
+    noiseMachineInfo.listNames.push(title);
+  });
+
+  const noiseMachineLinks = $(elem).find('.DIM a');
+  $(noiseMachineLinks).each(function(i, elem) {
+    const parentTitle = $(elem).parent('.generator-list').find('h1').text();
+    let categories = $(elem).className.split(/\s+/);
+    categories.pop(); // removes 'hint'
+    categories.pop(); // removes machine short name
+
+    const result = addNewItems(categories, noiseMachineInfo.noiseMachineCategories);
+    noiseMachineInfo.noiseMachineCategories = result.targetList;
+    noiseMachineInfo.noiseMachines.push({
+      id: i,
+      name: $(elem).text(),
+      href: $(elem).attr('href'),
+      categories: result.itemList // TODO: create a friendly mapping from class name to category name
+    });
+  });
 
   $(links).each(function(i, elem) {
     const sounds = $(elem).parent().next('ul._33MEMislY0GAlB78wL1_CR').find('li p').text().replace('Sounds Used: ', '').split(', ');
